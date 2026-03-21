8ea534ae562f9f5627c6315d6c6826962bf3492a
TabooWhisky <114479745+TabooWhisky@users.noreply.github.com>
2026-03-21 23:00:17 +0800

    Add files via upload

---
+++ MhachBLM/Translation.lua
@@ -0,0 +1,165 @@
+local tbl =
+{
+    ["QT"] =
+    {
+        [1] =
+        {
+            ["CN"] = "CD",
+            ["EN"] = "CD",
+            ["JP"] = "CD",
+        },
+        [2] =
+        {
+            ["CN"] = "DOT",
+            ["EN"] = "DOT",
+            ["JP"] = "DOT",
+        },
+        [3] =
+        {
+            ["CN"] = "AOE",
+            ["EN"] = "AOE",
+            ["JP"] = "AOE",
+        },
+        [4] =
+        {
+            ["CN"] = "爆发药",
+            ["EN"] = "Potion",
+            ["JP"] = "爆発薬",
+        },
+        [5] =
+        {
+            ["CN"] = "通晓",
+            ["EN"] = "Polyglot",
+            ["JP"] = "ポリグロット",
+        },
+        [6] =
+        {
+            ["CN"] = "黑魔纹",
+            ["EN"] = "Ley Lines",
+            ["JP"] = "黒魔紋",
+        },
+        [7] =
+        {
+            ["CN"] = "魔泉",
+            ["EN"] = "Manafont",
+            ["JP"] = "マナフォント",
+        },
+        [8] =
+        {
+            ["CN"] = "详述",
+            ["EN"] = "Amplifier",
+            ["JP"] = "アンプリファイア",
+        },
+        [9] =
+        {
+            ["CN"] = "三连咏唱",
+            ["EN"] = "Triplecast",
+            ["JP"] = "三連魔",
+        },
+        [10] =
+        {
+            ["CN"] = "燃尽爆发",
+            ["EN"] = "Burn",
+            ["JP"] = "燃え尽き爆発",
+        },
+        [11] =
+        {
+            ["CN"] = "智能目标",
+            ["EN"] = "Smart Target",
+            ["JP"] = "スマート目標",
+        },
+        [12] =
+        {
+            ["CN"] = "更多移动",
+            ["EN"] = "More Move",
+            ["JP"] = "もっと移動",
+        },
+    },
+
+    ["MSet"] =
+    {
+        [1] =
+        {
+            ["CN"] = "主设置",
+            ["EN"] = "MainSetting",
+            ["JP"] = "主設定",
+        },
+        [2] =
+        {
+            ["CN"] = "调试输出",
+            ["EN"] = "Debug Output",
+            ["JP"] = "デバッグ出力",
+        },
+        [3] =
+        {
+            ["CN"] = "我开启了动画锁",
+            ["EN"] = "I turned on animation lock",
+            ["JP"] = "私はアニメーションロックをオンにしました",
+        },
+        [4] =
+        {
+            ["CN"] = "强插能力技",
+            ["EN"] = "Force OGCD",
+            ["JP"] = "強制挿入能力技",
+        },
+        [5] =
+        {
+            ["CN"] = "魔纹步替补以太步",
+            ["EN"] = "Between the Lines Switch to Aetherial Manipulation",
+            ["JP"] = "ラインズステップ to エーテリアルステップ",
+        },
+        [6] =
+        {
+            ["CN"] = "显示接下来的技能",
+            ["EN"] = "Ley Show the next skill",
+            ["JP"] = "次のスキルを表示する",
+        },
+        [7] =
+        {
+            ["CN"] = "不知道干啥的就别点",
+            ["EN"] = "If you don't know what it does, don't click it.",
+            ["JP"] = "何をするか分からないなら、クリックしないで",
+        },
+        [8] =
+        {
+            ["CN"] = "战斗外禁用热键",
+            ["EN"] = "Disable hotkeys outside of combat",
+            ["JP"] = "戦闘外でホットキーを無効化",
+        },
+        [9] =
+        {
+            ["CN"] = "Dot目标黑名单",
+            ["EN"] = "Dot Target Blacklist",
+            ["JP"] = "Dotターゲットブラックリスト",
+        },
+        [10] =
+        {
+            ["CN"] = "输入需要加入黑名单的content id,以逗号分隔",
+            ["EN"] = "Enter the content IDs to be added to the blacklist, separated by commas",
+            ["JP"] = "ブラックリストに追加するコンテンツIDを入力してください。コンマで区切ってください。",
+        },
+    },
+
+    ["HSet"] =
+    {
+        [1] =
+        {
+            ["CN"] = "HotBar设置",
+            ["EN"] = "HotBarSetting",
+            ["JP"] = "ホットバー設定",
+        },
+        [2] =
+        {
+            ["CN"] = "显示热键栏",
+            ["EN"] = "Show hotbar Output",
+            ["JP"] = "ホットキー バーを表示",
+        },
+        [3] =
+        {
+            ["CN"] = "按下按键以绑定",
+            ["EN"] = "Press the button to bind",
+            ["JP"] = "キーを押してバインドする",
+        },
+    },
+}
+return tbl
